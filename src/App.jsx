b8f3747aa7282c2ea264a6169b071ecac23377e6
Anjali Vadukar <anjaliprajapati2636@gmail.com>
2026-02-02 13:32:54 +0530

    [A][Register form with navigate]

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
 // import { useState } from 'react'
-import './App.css'
- import Register from './pages/Register'
- import Login from './pages/Login'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import "./App.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 
+const DefaultRoute = () => {
+  const authData = JSON.parse(localStorage.getItem("authData"));
+  if (authData) {
+    return <Navigate to="/login" replace/>;
+  }
+  return <Navigate to="/register" replace />;
+};
 
 function App() {
-  const route= createBrowserRouter([
-{
-  path: '/login',
-  element: <Login />
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <DefaultRoute />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
 
-},
-{
-  path: '/register',
-  element:<Register />
+  return <RouterProvider router={route} />;
 }
 
-])
-
-  return <RouterProvider router={route} />
-}
-
-export default App
+export default App;
